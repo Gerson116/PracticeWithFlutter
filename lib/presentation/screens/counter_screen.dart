f269a21bdc00d971afabec932239a34cd8cddc02
gersondevx <gersondevx@gmail.com>
2024-07-10 06:58:52 -0400

    I was adding a new button in the header

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -14,44 +14,84 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mi Contador'),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 148, 179, 204),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              counter.toString(),
-              style: const TextStyle(fontSize: 75),
-            ),
-            Text(
-              clicks,
-              style: const TextStyle(fontSize: 25),
+        appBar: AppBar(
+          title: const Text(
+            'Mi Contador',
+            style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 247, 104, 93),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.restart_alt_rounded),
+              onPressed: () {
+                //...
+                setState(() {                  
+                  counter = 0;
+                  changeClickMessage(counter: counter);
+                });
+              },
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //...In this method or function, I can execute logic
-          setState(() {
-            counter += 1;
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                counter.toString(),
+                style: const TextStyle(fontSize: 75),
+              ),
+              Text(
+                clicks,
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                //...In this method or function, I can execute logic
+                setState(() {
+                  counter += 1;
+                  changeClickMessage(counter: counter);
+                });
+              },
+              backgroundColor: const Color.fromARGB(255, 247, 104, 93),
+              child: const Icon(
+                Icons.add_outlined,
+                color: Color.fromARGB(255, 248, 247, 247),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  counter -= 1;
+                  changeClickMessage(counter: counter);
+                });
+              },
+              backgroundColor: const Color.fromARGB(255, 247, 104, 93),
+              child: const Icon(
+                Icons.remove_outlined,
+                color: Color.fromARGB(255, 248, 247, 247),
+              ),
+            )
+          ],
+        ));
+  }
 
-            if (counter == 1) {
-              clicks = "Click";
-            }
-            if (counter > 1) {
-              clicks = "Clicks";
-            }
-          });
-          print("Quantity Click: $counter");
-        },
-        child: const Icon(Icons.plus_one),
-        backgroundColor: const Color.fromARGB(255, 248, 149, 142),
-      ),
-    );
+  void changeClickMessage({required int counter}) {
+    if (counter == 1) {
+      clicks = "Click";
+    }
+    if (counter == 0 || counter > 1) {
+      clicks = "Clicks";
+    }
   }
 }
